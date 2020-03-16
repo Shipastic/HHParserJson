2aef9e3e9dd4bf39f124bd47f00c2e4fb09842e4
Denis <denkul87@gmail.com>
2020-03-16 23:37:16 +0300

    Поиск информации по введеному названию

---
+++ HHLibrary/HHModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HHLibrary
+{
+    public class HHModel
+    {
+        /// <summary>
+        /// Получение ответа на GET запрос
+        /// </summary>
+        /// <param name="request">URL запроса</param>
+        /// <returns></returns>
+        public string RequestGet(string request )
+        {
+            string tmp = "";
+            try
+            {
+                WebClient client = new WebClient();
+
+                client.Headers.Add("user-agent", "Chrome");
+
+                Stream stream = client.OpenRead(request);
+
+                StreamReader sr = new StreamReader(stream);
+
+                string line = "";
+
+                while ((line = sr.ReadLine()) != null)
+
+                    tmp += line;
+
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                tmp = ex.Message;
+            }
+            return tmp;
+        }
+        //Парсинг Json
+        public string JsonParseString(string json)
+        {
+            string tmp = "";
+            dynamic obj = JObject.Parse(json);
+
+            tmp += "Должность: " + obj.name + "\r\n";
+            tmp += "Зарплата от " + obj.salary.from + " до " + obj.salary.to + " " + obj.salary.currency + "\r\n";
+            tmp += "Адресс работы: город " + obj.address.city + ", " + obj.address.street +"," + " дом №: "+ obj.address.building + "\r\n";
+            return tmp;
+        }
+    }
+}
